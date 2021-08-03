66f9202fdf737ab3c6c45a3addc022fd31ca6872
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2021-08-03 08:41:18 +0300

    началаделать слайдеры

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,22 @@
-console.log('script work!');
+$(document).ready(function() {
+    console.log('script work!');
+
+    // Делаем карусель в секции photos
+    $('.js-slider-wrap').each(function(index, sliderWrap) {
+        let slider = $(sliderWrap).find('.js-slider');
+        let btnPrev = $(sliderWrap).find('.js-btn-prev');
+        let btnNext = $(sliderWrap).find('.js-btn-next');
+
+        $(slider).slick({
+            // autoplay: true,
+            // dots: true,
+            prevArrow: btnPrev,
+            nextArrow: btnNext,
+        });
+
+        console.log(index);
+        console.log(sliderWrap);
+    });
+
+
+});
